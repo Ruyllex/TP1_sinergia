bba6a80b98d4cd28687414a1971be1dbd0866b51
ZoiloPazos <zpazos@fi.uba.ar>
2021-06-21 03:20:30 -0300

    resuelto en el archivo entregado que se pueda salir con FIN

--- correccion.py
+++ correccion.py
@@ -2,7 +2,7 @@
 
 """
     Integrantes:
-        
+
     -Jorge Sedek
     -Zoilo Pazos
     -Joaquín Maguiña
@@ -11,7 +11,7 @@
     -Agustín Sánchez Vergara
 
     Fecha: 1° Cuatrimestre del 2021
-    
+
 """
 #---------IMPORTES--------------------------#
 from texto import obtener_texto
@@ -23,7 +23,6 @@ def main():
 
     #CORRECCION: El main puede modularizarse más
     #CORRECCION: Regla de Oro: si un código necesita comentarios para que se entendible, algo estamos haciendo mal
-
     intento = 1
     puntaje_total = 0
     respuesta = "si" # CORRECCION: Esto no es una pregunta, es una respuesta. Mal nombre de variable
@@ -50,7 +49,7 @@ def main():
         longitud_palabra_elegida = validar_longitud_palabra(longitud_palabra_elegida)
         palabra_adivinar = eliminar_tildes(elegir_palabra(diccionario, longitud_palabra_elegida))
 
-        
+
 
         #Inicialización de variables
         desaciertos = 0
@@ -58,10 +57,10 @@ def main():
         lista_aciertos_desaciertos = [aciertos,desaciertos]
         cadena_letras_incorrectas = ""
         cadena_letras_repetidas = ""
-        palabra_secreta = esconder_palabra(palabra_adivinar) 
-        
-        lista_secreta = list(palabra_secreta)  
-        cadena_secreta = list(lista_secreta) 
+        palabra_secreta = esconder_palabra(palabra_adivinar)
+
+        lista_secreta = list(palabra_secreta)
+        cadena_secreta = list(lista_secreta)
 
         #Se le pregunta por 1era vez al usuario que adivine una letra
 
@@ -74,35 +73,31 @@ def main():
         letra = input("Ingresar letra: ")
         letra_validada = validar_letra(letra)
         letra_verificada = verificar_repetido(letra_validada,cadena_letras_repetidas)
-            
-        while letra_verificada not in ("0","FIN") and lista_aciertos_desaciertos[1] < 8 and cadena_secreta != palabra_adivinar:
-            
+
+        while letra_verificada not in ("FIN", "0") and lista_aciertos_desaciertos[1] < 8 and cadena_secreta != palabra_adivinar:
+
             letra_verificada = letra_verificada.lower()
             cadena_letras_repetidas += letra_verificada 
             es_acierto = letra_verificada in palabra_adivinar
-            
+
             lista_aciertos_desaciertos = contador_aciertos_desaciertos(es_acierto, lista_aciertos_desaciertos)
-            
+
             if es_acierto:
                 lista_posicion = posicion_letra_en_palabra(letra_verificada, palabra_adivinar) #lista con las posiciones que esta la letra
                 lista_secreta  = ingresar_letra_en_lista_secreta(letra_verificada, lista_secreta, lista_posicion) #cambia listas de ???? x ?a?a
-                
-            
+
+
             else:
                 cadena_letras_incorrectas += letra_verificada
-                
+
             cadena_secreta = lista_a_cadena(lista_secreta)
             if lista_aciertos_desaciertos[1] < 8 and cadena_secreta != palabra_adivinar: # CORRECCION: Usar constantes
                 mostrar_mensaje(mostrar_mensaje_progreso(es_acierto), cadena_secreta, lista_aciertos_desaciertos[0], lista_aciertos_desaciertos[1], cadena_letras_incorrectas) # CORRECCION: Usar constantes
                 letra = input("Ingresar letra: ")
                 letra_validada = validar_letra(letra)
                 letra_verificada = permitir_letra(letra_validada,cadena_letras_repetidas)
-            
-                
-            
-                
-            
-            
+
+
         ultimo_mensaje(cadena_secreta,palabra_adivinar,lista_aciertos_desaciertos[0], lista_aciertos_desaciertos[1],cadena_letras_incorrectas)
         print("\nTu puntaje fue: ",contar_puntajes(lista_aciertos_desaciertos[0], lista_aciertos_desaciertos[1]))
 
@@ -122,30 +117,3 @@ main()
 # CORRECCION: MUY IMPORTANTE! Se estan tomando muy en serio lo de que cada función haga una única cosa, se lo estan tomando muy literal. Tienen que hacerte esta pregunta "Que hace esta función?".
 # Si la respuesta es de la forma: "la función hace esto Y esto" entonces hay algo mal (por ejemplo: la función pide ingreso al usuario Y valida ese ingreso) (otro ejemplo en base a lo que pusieron
 # en la linea 169: la función devuelve un texto en minúscula sin caracteres especiales  <-- está bien)
-
-            
-            
-                
-            
-
-            
-            
-            
-
-            
-            
-            
-            
-           
-                    
-            
-            
-        
-
-
-
-
-
-
-
-
