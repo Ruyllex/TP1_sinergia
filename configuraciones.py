6ab57c0379c0f087d66e20150483597c194d319f
Ruyllex <75554521+Ruyllex@users.noreply.github.com>
2021-07-05 14:43:08 -0300

    puse espacios

--- configuraciones.py
+++ configuraciones.py
@@ -11,29 +11,29 @@ diccionario_default={'MAX_USUARIOS': 10, 'LONG_PALABRA_MIN': 5, 'MAX_DESACIERTOS
 
 def constantes():
     '''retorna un diccionario con las constantes del configuracion.csv'''
-    configuraciones=open("configuracion.csv","rt")
+    configuraciones = open("configuracion.csv","rt")
     linea=leer_lineas(configuraciones)
-    diccionario={}
-    while linea!='':
+    diccionario = {}
+    while linea != '':
         linea=linea.split(",")
         try:
-            diccionario[linea[0]]=int(linea[1])
-            linea=leer_lineas(configuraciones)
+            diccionario[linea[0]] = int(linea[1])
+            linea = leer_lineas(configuraciones)
         except IndexError:
-            diccionario=diccionario_default
-            linea=''
+            diccionario = diccionario_default
+            linea = ''
         except ValueError:
-            diccionario=diccionario_default
-            linea=''
+            diccionario = diccionario_default
+            linea = ''
     configuraciones.close()
     if diccionario_default.keys() != diccionario.keys():
-        diccionario=diccionario_default
+        diccionario = diccionario_default
     return diccionario
 def check(diccionario):
     '''chequea si el diccionario es distinto al default'''
-    if diccionario!=diccionario_default:
-        mensaje="Los valores por defecto en el archivo han sido cambiados"
+    if diccionario != diccionario_default:
+        mensaje = "Los valores por defecto en el archivo han sido cambiados"
     else:
-        mensaje="Se mantienen los valores por defecto"
+        mensaje = "Se mantienen los valores por defecto"
     return mensaje
 
