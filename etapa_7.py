15f599c98955e2ca661a00b15a98f1c67952917c
Ruyllex <75554521+Ruyllex@users.noreply.github.com>
2021-07-05 17:55:23 -0300

    ruy

--- etapa_7.py
+++ etapa_7.py
@@ -15,7 +15,6 @@ def turno_de_un_jugador(nombre,dicc_repetidas_incorrectas,dicc_palabra_adivinar_
     es_acierto = True
     cadena_secreta = "".join(dicc_palabra_adivinar_e_secreta[nombre][1])
     while es_acierto and not descubrio_palabra(nombre,dicc_palabra_adivinar_e_secreta):
-        mostrar_mensaje(mostrar_mensaje_progreso(es_acierto), cadena_secreta, dicc_aciertos_desaciertos[nombre][0], dicc_aciertos_desaciertos[nombre][1], dicc_repetidas_incorrectas[nombre][1])
         letra = input(f"{nombre} → Ingresar letra: ")
         valida = validar_letra(letra)
         repetida = verificar_repetido(letra,dicc_repetidas_incorrectas[nombre][0])
@@ -26,6 +25,7 @@ def turno_de_un_jugador(nombre,dicc_repetidas_incorrectas,dicc_palabra_adivinar_
         dicc_repetidas_incorrectas[nombre][0] += letra_verificada
         
         es_acierto = letra_verificada in dicc_palabra_adivinar_e_secreta[nombre][0] 
+        mostrar_mensaje(mostrar_mensaje_progreso(es_acierto), cadena_secreta, dicc_aciertos_desaciertos[nombre][0], dicc_aciertos_desaciertos[nombre][1], dicc_repetidas_incorrectas[nombre][1])
         dicc_aciertos_desaciertos[nombre] = contador_aciertos_desaciertos(es_acierto, dicc_aciertos_desaciertos[nombre])
 
         if es_acierto:
