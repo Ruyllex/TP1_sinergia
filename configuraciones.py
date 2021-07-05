c9f40e3d58e5130999ba7e254d44fefc70d93ebf
Ruyllex <75554521+Ruyllex@users.noreply.github.com>
2021-07-04 23:31:33 -0300

    constantes

--- configuraciones.py
+++ configuraciones.py
@@ -1,1 +1,22 @@
+#-----------CONFIGURACIONES----------------
+def leer_lineas(archivo):
+    linea = archivo.readline()
+    linea = linea.rstrip('\n')
+    if linea:
+        devolucion = linea
+    else: devolucion = ''
+    return devolucion
+
+def constantes():
+    '''returna diccionario con las constantes del configuracion.csv'''
+    configuraciones=open("configuracion.csv","rt")
+    linea=leer_lineas(configuraciones)
+    dicc={}
+    while linea!='':
+        linea=linea.split(",")
+        dicc[linea[0]]=int(linea[1])
+        linea=leer_lineas(configuraciones)
+    configuraciones.close()
+    return dicc
+
 
